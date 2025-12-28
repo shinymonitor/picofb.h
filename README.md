3d74a5285b5053bb89d7ad437426b382700ded80
Arin Upadhyay <arinupadhyay.cs@gmail.com>
2025-12-28 02:01:50 +0100

    INIT

---
+++ README.md
@@ -0,0 +1,29 @@
+# picofb
+
+A minimal single-header framebuffer library for quickly getting pixels on screen with multi input handling
+
+## Features
+
+- Simple API: init, set pixels, update, cleanup
+- Two implementations (API compatible with both):
+  - **X11**: Faster performance (~2x FPS), Linux/X11 only
+  - **SDL2**: Cross-platform (Linux/Windows/Mac)
+- Save framebuffer to PPM image format
+- Configurable resolution via compile-time defines
+
+## Use cases
+- Software rendered applications (UI, Games)
+- Easy render testing
+
+## Usage
+See `example.c`
+
+## Compilation
+
+```bash
+gcc example.c -o example -lSDL2
+```
+or define `PICOFB_X11_BACKEND` use X11 
+```bash
+gcc example.c -o example -lX11 
+```
---
+++ example.c
@@ -0,0 +1,43 @@
+//Define screen dimensions
+#define PICOFB_WIDTH 640
+#define PICOFB_HEIGHT 360
+
+//Define this macro to use x11 backend
+//#define PICOFB_X11_BACKEND
+#include "picofb.h"
+
+int main(){
+    //Allocate window
+    PICOFB_Window picofb_window={0};
+
+    //Initialize the window
+    if (!PICOFB_init("EXAMPLE", &picofb_window)) {fprintf(stderr, "init failed\n"); return 1;}
+
+    //modify frame buffer before main loop (gradient)
+    for (int y = 0; y < PICOFB_HEIGHT; ++y) for (int x = 0; x < PICOFB_WIDTH; ++x) PICOFB_set_pixel(&picofb_window, x, y, PICOFB_pixel_rgb((x * 255) / PICOFB_WIDTH, (y * 255) / PICOFB_HEIGHT, ((x + y) * 255) / (PICOFB_WIDTH + PICOFB_HEIGHT)));
+
+    //loop until quit
+    while (!picofb_window.quit) {
+        //modify frame buffer during main loop
+        //set a pixel to a rgb color
+        PICOFB_set_pixel(&picofb_window, PICOFB_WIDTH/2, PICOFB_HEIGHT/2, PICOFB_pixel_rgb(255, 255, 255));
+
+        //update the window
+        PICOFB_update(&picofb_window);
+
+        //multikey input
+        if (PICOFB_is_input(&picofb_window, PICOFB_Key_w)) printf("w");
+        if (PICOFB_is_input(&picofb_window, PICOFB_Key_s)) printf("s");
+        if (PICOFB_is_input(&picofb_window, PICOFB_Key_a)) printf("a");
+        if (PICOFB_is_input(&picofb_window, PICOFB_Key_d)) printf("d");
+        printf("\n");
+    }
+
+    //save last frame to ppm
+    PICOFB_save_ppm(&picofb_window, "example.ppm");
+
+    //cleanup window
+    PICOFB_cleanup(&picofb_window);
+    
+    return 0;
+}
