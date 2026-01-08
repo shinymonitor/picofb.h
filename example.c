33f47dc2422fe3e1881b559b912d9dd483971446
Arin Upadhyay <arinupadhyay.cs@gmail.com>
2026-01-08 23:03:06 +0100

    api change

--- example.c
+++ example.c
@@ -1,24 +1,25 @@
 //Define screen dimensions
-#define PICOFB_WIDTH 640
-#define PICOFB_HEIGHT 360
+#define WIDTH 640
+#define HEIGHT 360
 
 #include "picofb.h"
 
 int main(){
     //Allocate window
     PICOFB_Window picofb_window={0};
+    uint32_t fb[WIDTH * HEIGHT]={0};
 
     //Initialize the window
-    if (!PICOFB_init("EXAMPLE", &picofb_window)) {fprintf(stderr, "init failed\n"); return 1;}
+    if (!PICOFB_init("EXAMPLE", WIDTH, HEIGHT, fb, &picofb_window)) {fprintf(stderr, "init failed\n"); return 1;}
 
     //modify frame buffer before main loop (gradient)
-    for (int y = 0; y < PICOFB_HEIGHT; ++y) for (int x = 0; x < PICOFB_WIDTH; ++x) PICOFB_set_pixel(&picofb_window, x, y, PICOFB_color_rgb((x * 255) / PICOFB_WIDTH, (y * 255) / PICOFB_HEIGHT, ((x + y) * 255) / (PICOFB_WIDTH + PICOFB_HEIGHT)));
+    for (size_t y = 0; y < HEIGHT; ++y) for (size_t x = 0; x < WIDTH; ++x) PICOFB_set_pixel(&picofb_window, x, y, PICOFB_color_rgb((x * 255) / WIDTH, (y * 255) / HEIGHT, ((x + y) * 255) / (WIDTH + HEIGHT)));
 
     //loop until quit
     while (!picofb_window.quit) {
         //modify frame buffer during main loop
         //set a pixel to a rgb color
-        PICOFB_set_pixel(&picofb_window, PICOFB_WIDTH/2, PICOFB_HEIGHT/2, PICOFB_color_rgb(255, 255, 255));
+        PICOFB_set_pixel(&picofb_window, WIDTH/2, HEIGHT/2, PICOFB_color_rgb(255, 255, 255));
 
         //update the window
         PICOFB_update(&picofb_window);
@@ -28,7 +29,7 @@ int main(){
         if (PICOFB_is_input(&picofb_window, PICOFB_Key_s)) printf("s");
         if (PICOFB_is_input(&picofb_window, PICOFB_Key_a)) printf("a");
         if (PICOFB_is_input(&picofb_window, PICOFB_Key_d)) printf("d");
-        if (PICOFB_is_input(&picofb_window, PICOFB_Key_q)) break;
+        if (PICOFB_is_input(&picofb_window, PICOFB_Key_q) || PICOFB_is_input(&picofb_window, PICOFB_Key_ESC)) break;
         printf("\n");
     }
 
